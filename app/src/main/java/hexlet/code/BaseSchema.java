73d6a7fcea1f683fe095b173dadaa09e0229d77b
Николай Малютин <xenos-nick@mail.ru>
2025-10-18 10:56:05 +0300

    refactor: Transfer the general validation logic to the BaseSchema class

---
+++ app/src/main/java/hexlet/code/BaseSchema.java
@@ -0,0 +1,25 @@
+package hexlet.code;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public class BaseSchema<T> {
+    private final Map<String, Predicate<T>> validations = new HashMap<>();
+
+    protected void addValidation(String name, Predicate<T> validation) {
+        validations.put(name, validation);
+    };
+
+    public boolean isValid(T value) {
+        if (value == null && validations.containsKey("required")) {
+            return false;
+        }
+        if (value == null) {
+            return true;
+        }
+
+        return validations.values().stream()
+                .allMatch(validation -> validation.test(value));
+    }
+}
--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,42 +1,27 @@
 package hexlet.code;
 
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.function.Predicate;
 
-public class StringSchema {
-    private final Map<String, Predicate<String>> validations = new TreeMap<>();
+public class StringSchema extends BaseSchema<String> {
 
     public StringSchema required() {
         String schemaKey = "required";
         Predicate<String> validation = value -> value != null && !value.isEmpty();
-        this.validations.put(schemaKey, validation);
+        addValidation(schemaKey, validation);
         return this;
     }
 
     public StringSchema minLength(int minLength) {
         String schemaKey = "minLength";
         Predicate<String> validation = value -> value.length() >= minLength;
-        this.validations.put(schemaKey, validation);
+        addValidation(schemaKey, validation);
         return this;
     }
 
     public StringSchema contains(String string) {
         String schemaKey = "contains";
         Predicate<String> validation = value -> value.contains(string);
-        this.validations.put(schemaKey, validation);
+        addValidation(schemaKey, validation);
         return this;
-    }
-
-    public boolean isValid(String s) {
-        if ((s == null) && validations.containsKey("required")) {
-            return false;
-        }
-        if (s == null) {
-            return true;
-        }
-
-        return validations.values().stream()
-                .allMatch(validation -> validation.test(s));
     }
 }
